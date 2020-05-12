0ff5a5a72630dce30201837f2e4b17ad574b1d29
Viktor Riga <rigavv@gmail.com>
2020-05-12 15:12:50 +0300

    start

---
+++ Assignment-2.-Block-2./Assignment-2.-Block-2./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment-2.-Block-2.
+//
+//  Created by Viktor Riga on 12.05.2020.
+//  Copyright © 2020 Viktor Riga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
